94069429863b742212bc51938815203d39d1bfdf
emtpy61 <120124822+emtpy61@users.noreply.github.com>
2023-06-06 18:46:03 +0300

    Pre sprint 11 (#4)
    * feat:Промежуточное задание месяца SQL
    
    * feat:Промежуточное задание месяца SQL
    
    * feat:Промежуточное задание месяца SQL

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder(toBuilder = true)
+public class Genre {
+    private int id;
+
+    @NotNull(message = "не может быть null.")
+    @NotBlank(message = "не должно быть пустым.")
+    private String name;
+}
